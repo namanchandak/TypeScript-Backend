47ac1ca4c21b7e6f84bcaab81fd1c26718b55d8f
namanchandak <naman.chandak@hotmail.com>
2024-03-24 19:16:23 +0530

    db created

--- lib/type_script-stack.ts
+++ lib/type_script-stack.ts
@@ -2,21 +2,24 @@ import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
-
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
 
 export class TypeScriptStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_16_X,    // execution environment
-      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 'hello.handler'                // file is "hello", function is "handler"
+ 
+    const table = new dynamodb.Table(this, 'DynamoDbSample', {
+      partitionKey: {
+        name: 'users',
+        type: dynamodb.AttributeType.STRING
+      }, 
+      sortKey: {
+        name: 'created_at',
+        type: dynamodb.AttributeType.NUMBER
+      },
     });
-
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
-    });
+    new CfnOutput(this, 'TableName', { value: table.tableName });
 
   }
 }
