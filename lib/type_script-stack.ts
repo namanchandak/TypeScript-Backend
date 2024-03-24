f86a700489e529ab1631e8c73a31e981ffd037cb
namanchandak <naman.chandak@hotmail.com>
2024-03-24 18:49:12 +0530

    api, lambda fun, deploy

--- lib/type_script-stack.ts
+++ lib/type_script-stack.ts
@@ -1,19 +1,22 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+
 
 export class TypeScriptStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'TypeScriptQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'TypeScriptTopic');
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
